a92251bf47a5f96eae0092e94cf179ec8f988d6a
Ashwanth Balakrishnan <ashwanthbalakrishnan5@gmail.com>
2025-01-23 16:41:38 +0530

    Reorder URL patterns to include router URLs after custom user settings path

--- apps/authw/urls.py
+++ apps/authw/urls.py
@@ -9,12 +9,12 @@ router.register("auth/user", UserListViewSet, basename="users")
 router.register("auth/settings", UserSettingsViewSet, basename="settings")
 
 urlpatterns = [
-    path("v1/", include(router.urls)),
     path(
         "v1/auth/settings/",
         UserSettingsViewSet.as_view({"patch": "custom_partial_update"}),
         name="user-settings-partial-update",
     ),
+    path("v1/", include(router.urls)),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
